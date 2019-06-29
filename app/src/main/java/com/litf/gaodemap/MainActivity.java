152fead57055ab97d546425f649f791bc21e9d0c
ltf <893305119@qq.com>
2019-06-29 18:35:22 +0800

    1

--- app/src/main/java/com/litf/gaodemap/MainActivity.java
+++ app/src/main/java/com/litf/gaodemap/MainActivity.java
@@ -2,20 +2,33 @@ package com.litf.gaodemap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.Circle;
+import com.amap.api.maps.model.CircleOptions;
 import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.Marker;
 import com.amap.api.maps.model.MarkerOptions;
 import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.maps.model.Polyline;
+import com.amap.api.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private  MapView mMapView = null;
     private AMap aMap;
     private MyLocationStyle myLocationStyle;
+    private Polyline polyline;
+    private Circle circle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +41,69 @@ public class MainActivity extends AppCompatActivity {
         }
 
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
+        /**
+         * myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);//只定位一次。
+         * myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE) ;//定位一次，且将视角移动到地图中心点。
+         * myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW) ;//连续定位、且将视角移动到地图中心点，定位蓝点跟随设备移动。（1秒1次定位）
+         * myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE);//连续定位、且将视角移动到地图中心点，地图依照设备方向旋转，定位点会跟随设备移动。（1秒1次定位）
+         * myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
+         * //以下三种模式从5.1.0版本开始提供
+         * myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，定位点依照设备方向旋转，并且蓝点会跟随设备移动。
+         * myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_FOLLOW_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，并且蓝点会跟随设备移动。
+         * myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_MAP_ROTATE_NO_CENTER);//连续定位、蓝点不会移动到地图中心点，地图依照设备方向旋转，并且蓝点会跟随设备移动。
+         */
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
-        myLocationStyle.showMyLocation(true);
+        myLocationStyle.showMyLocation(true);//设置是否显示定位小蓝点，用于满足只想使用定位，不想使用定位小蓝点的场景，设置false以后图面上不再有定位蓝点的概念，但是会持续回调位置信息。
         myLocationStyle.strokeWidth(50);//设置定位蓝点精度圈的边框宽度的方法。
         aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         aMap.moveCamera(CameraUpdateFactory.zoomTo(12));
 
+        aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                ToastUtils.showToast(MainActivity.this,location.getLongitude()+","+location.getLatitude());
+            }
+        });
+
+        //绘制标记点marker
         LatLng latLng = new LatLng(39.906901,116.397972);
-        final Marker marker = aMap.addMarker(new MarkerOptions().position(latLng).title("北京4444").snippet("DefaultMarker"));
+        final Marker marker1 = aMap.addMarker(new MarkerOptions().position(new LatLng(39.906901,116.397972)).title("标记1").snippet("内容1"));
+//        final Marker marker2 = aMap.addMarker(new MarkerOptions().position(new LatLng(39.917930,116.498993)).title("标记2").snippet("内容2"));
+
+        //自定义标记图标marker
+        MarkerOptions markerOption = new MarkerOptions();
+//        markerOption.position(Constants.XIAN);
+        markerOption.position(new LatLng(39.926901,116.527972));
+        markerOption.title("标记3");
+
+        markerOption.draggable(false);//设置Marker可拖动
+        markerOption.icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory
+                .decodeResource(getResources(),R.drawable.ic_launcher)));
+        // 将Marker设置为贴地显示，可以双指下拉地图查看效果
+        markerOption.setFlat(true);//设置marker平贴地图效果
+
+        final Marker marker3 = aMap.addMarker(markerOption);
+////绘制面
+        LatLng latLng2 = new LatLng(39.917930,116.498993);
+        circle = aMap.addCircle(new CircleOptions().
+                center(latLng2).
+                radius(1000).
+                fillColor(R.color.main_nav_text).
+                fillColor(Color.argb(255, 255, 255, 1)).
+                strokeColor(Color.argb(255, 255, 1, 1)).
+                strokeWidth(15));
+
+        //绘制标记线
+        List<LatLng> latLngs = new ArrayList<LatLng>();
+        latLngs.add(new LatLng(39.906901,116.397972));
+        latLngs.add(new LatLng(39.917930,116.498993));
+        latLngs.add(new LatLng(39.926901,116.527972));
+//        latLngs.add(new LatLng(39.955192,116.140092));
+        polyline =aMap.addPolyline(new PolylineOptions().
+                addAll(latLngs).width(10).color(Color.argb(255, 0, 1, 0)));
     }
     @Override
     protected void onDestroy() {
---
+++ app/src/main/java/com/litf/gaodemap/ToastUtils.java
@@ -0,0 +1,10 @@
+package com.litf.gaodemap;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtils {
+    public static void showToast(Context context, String string){
+        Toast.makeText(context,string, Toast.LENGTH_SHORT).show();
+    }
+}
